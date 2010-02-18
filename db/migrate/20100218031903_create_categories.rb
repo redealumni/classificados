a7d2b257ebd2231f34c027832c83849928ed011b
Bernardo de Pádua <berpasan@gmail.com>
2010-02-18 02:09:37 -0200

    Melhor não adicionar a coluna ads_count agora.

--- db/migrate/20100218031903_create_categories.rb
+++ db/migrate/20100218031903_create_categories.rb
@@ -2,8 +2,6 @@ class CreateCategories < ActiveRecord::Migration
   def self.up
     create_table :categories do |t|
       t.string :name
-      t.integer :ads_count, :default => 0
-
       t.timestamps
     end
   end
