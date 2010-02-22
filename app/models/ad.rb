1ba7b06b36565949f9f58903585b7b1aa76b5308
Bernardo de Pádua <berpasan@gmail.com>
2010-02-22 00:39:57 -0300

    Envia e-mail de confirmação e gera código secreto do anúncio.

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,4 +1,8 @@
+require 'digest/sha1'
 class Ad < ActiveRecord::Base
+  
+  before_create :generate_secret_code, :send_confirmation_email
+  
   KINDS = {:buy => "compra", :sell => "venda", :exchange => "troca"}
   
   named_scope :ordered_by_creation, :order => "created_at DESC"
@@ -13,5 +17,15 @@ class Ad < ActiveRecord::Base
   validates_length_of :title, :within => 5..50
   validates_length_of :body, :within => 20..500
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/
+  
+  protected
+
+  def send_confirmation_email
+    Mailer.deliver_ad_creation(self)
+  end
+  
+  def generate_secret_code
+    self.secret_code = Digest::SHA1.hexdigest("EsteConteúdoDeveSerSuperSecreto!!!#{Time.now.to_s.split(//).sort_by {rand}}#{self.title}#{self.email}")
+  end
   
 end
--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -1,13 +1,15 @@
 class Mailer < ActionMailer::Base
   
 
-  def ad_creation(sent_at = Time.now)
-    subject    'Mailer#ad_creation'
-    recipients ''
-    from       ''
-    sent_on    sent_at
+  def ad_creation(ad)
+    subject    "Importante - Anúncio '#{ad.title}' criado"
+    recipients ad.email
+    from       "no-reply@example.org"
+    sent_on    Time.now
     
-    body       :greeting => 'Hi,'
+    @ad = ad
+    @created_at = Time.now
+    
   end
 
 end
