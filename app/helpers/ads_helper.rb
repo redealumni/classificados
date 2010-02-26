574b93bef78249693879b915d09b0b56ad35d57a
Bernardo de Pádua <berpasan@gmail.com>
2010-02-25 22:43:03 -0300

    Exibe texto quando não tiver resultados para a busca

--- app/helpers/ads_helper.rb
+++ app/helpers/ads_helper.rb
@@ -1,9 +1,14 @@
 module AdsHelper
   def ads_summary_as_sentence
-    [ 
+    strings = [ 
       t(:number_of_ads_by_type, :count => @selling.size , :type => t(:sell)) ,
       t(:number_of_ads_by_type, :count => @buying.size , :type => t(:buy)) ,
       t(:number_of_ads_by_type, :count => @exchanging.size , :type => t(:exchange))
-    ].compact.to_sentence
+    ].compact
+    if strings.empty?
+      t(:no_results_for_you)
+    else
+       strings.to_sentence
+    end
   end
 end
